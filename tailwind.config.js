595d4ed252a3da46fe94fc3c0091931bbe2a07e5
Rıza Baran Boğa <avbaranboga@gmail.com>
2025-11-21 21:55:28 +0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       animation: {
-        shimmer: 'shimmer 2s infinite',
+        shimmer: "shimmer 2s infinite",
       },
       keyframes: {
         shimmer: {
-          '0%': { backgroundPosition: '-1000px 0' },
-          '100%': { backgroundPosition: '1000px 0' },
+          "0%": { backgroundPosition: "-1000px 0" },
+          "100%": { backgroundPosition: "1000px 0" },
         },
       },
     },
   },
   plugins: [],
-}
-
+};
